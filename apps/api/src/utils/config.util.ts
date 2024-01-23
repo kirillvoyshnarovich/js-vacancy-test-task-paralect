a90ceed81c4184bd2287917e72e24da8462deffb
Kirill Voiyshnarovich <kirill.v.ext@dataseer.digital>
2024-01-23 16:50:23 +0300

    remove console

--- apps/api/src/utils/config.util.ts
+++ apps/api/src/utils/config.util.ts
@@ -2,7 +2,6 @@ import { ZodSchema } from 'zod';
 
 const validateConfig = <T>(schema: ZodSchema): T => {
   const parsed = schema.safeParse(process.env);
-  console.log('process.env ---------------------------------> ', process.env);
   if (!parsed.success) {
     console.error('❌ Invalid environment variables:', parsed.error.flatten().fieldErrors);
 
