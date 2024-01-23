b6fb9d77968cdec3954843467f33992fe196f3eb
Kirill Voiyshnarovich <kirill.v.ext@dataseer.digital>
2024-01-23 22:07:38 +0300

    log db connection

--- apps/api/src/db.ts
+++ apps/api/src/db.ts
@@ -1,10 +1,14 @@
 import { Database, Service, ServiceOptions, IDocument } from '@paralect/node-mongo';
 
 import config from 'config';
-
+console.log('config.MONGO_URI', config.MONGO_URI);
 const database = new Database(config.MONGO_URI, config.MONGO_DB_NAME);
+try {
+  database.connect();
+} catch (e) {
+  console.log('connect e', e);
+}
 
-database.connect();
 
 class CustomService<T extends IDocument> extends Service<T> {
   // You can add new methods or override existing here
